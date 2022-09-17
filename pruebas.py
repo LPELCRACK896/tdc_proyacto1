0842fcc239a63799c397fdb148986043174105e4
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2022-09-16 20:37:56 -0600

    pruebas para escribir txt

---
+++ pruebas.py
@@ -0,0 +1,71 @@
+from types import TracebackType
+from AFN import AFN
+from AFD import AFD
+import AFN_to_AFD_transformer as afdC
+
+afn = AFN('A', ['0', '1', 'ε'], ['A', 'B', 'C', 'D', 'E', 'F'], ['D'], 
+            {
+                'A':
+                    {
+                        '0': ['E'], 
+                        '1': ['B'],
+                        'ε': None
+                    },
+                'B':
+                    {
+                        '0': None, 
+                        '1': ['C'],
+                        'ε': ['D']
+                    },
+                'C':
+                    {
+                        '0': None, 
+                        '1': ['D'],
+                        'ε': None
+                    },
+                'D':
+                    {
+                        '0': None, 
+                        '1': None,
+                        'ε': None
+                    },
+                'E':
+                    {
+                        '0': ['F'], 
+                        '1': None,
+                        'ε': ['B', 'C']
+                    },
+                'F':
+                    {
+                        '0': ['D'], 
+                        '1': None,
+                        'ε': None
+                    },
+            })
+afn2 = AFN('A', ['0', '1', 'ε'], ['A', 'B', 'C'], ['C'], 
+            {
+                'A':
+                    {
+                        '0': ['B'], 
+                        '1': ['B'],
+                        'ε': ['B']
+                    },
+                'B':
+                    {
+                        '0': None, 
+                        '1': ['A'],
+                        'ε': ['A']
+                    },
+                'C':
+                    {
+                        '0': ['A'], 
+                        '1': ['B'],
+                        'ε': None
+                    },
+            }
+)
+
+afn.create_cerradura_de_estados()
+afn.recorrido_AFN('000')
+
+afd = afdC.AFN_to_AFD_transformer(afn)
---
+++ pruebas_de_afd_afn_a_txt.py
@@ -0,0 +1,40 @@
+from AFD import AFD
+from AFN import AFN
+r = '(1|0)*'
+w = '10000'
+''''
+logica para obetener de regex a un afn o afd
+
+
+'''
+afd = AFD('A', ['1', '0'], ['A', 'B'], ['B'],{
+                'A':
+                    {
+                        '0': 'B', 
+                        '1': 'A'
+                    },
+                'B':
+                    {
+                        '0': 'B', 
+                        '1': 'A'
+                    },})
+#print(afd.transitions)
+
+
+
+def writeAFD(afd: AFD):
+    trans = "Transitions: "
+    for r in afd.transitions:
+        for b in afd.transitions.get(r):
+            trans += (f"({r}, {b}, {afd.transitions.get(r).get(b)})- ")
+    print(f"Estados =  {afd.estados}")
+    print(afd.alfabeto)
+    print(afd.estado_inicial)
+    print(afd.estados_de_aceptacion)
+    print(trans)
+
+writeAFD(afd)
+#devuelve = afd.emulate_AFD(w)
+
+
+#print(devuelve)
