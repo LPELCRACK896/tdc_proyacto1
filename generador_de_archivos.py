a2dd75b7e8a6b4be3b47ae559794e8f18384dc4d
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2022-09-28 17:34:56 -0600

    created functions to trun afd and afn to string

--- generador_de_archivos.py
+++ generador_de_archivos.py
@@ -11,6 +11,21 @@ def writeAFD(afd: AFD):
     
     f.write('Estados: '+str(afd.estados)+'\n'+'Simbolos: '+str(afd.alfabeto)+'\n'+'Estado inicial: '+str(afd.estado_inicial)+'\n'+'Aceptacion: '+str(afd.estados_de_aceptacion)+'\n'+'Transiciones: '+str(trans)+'\n')
 
+def AFD_toString(afd: AFD):
+    trans = ""
+    for r in afd.transitions:
+        for b in afd.transitions.get(r):
+            trans += (f"({r}, {b}, {afd.transitions.get(r).get(b)})- ")
+      
+    return 'Estados: '+str(afd.estados)+'\n'+'Simbolos: '+str(afd.alfabeto)+'\n'+'Estado inicial: '+str(afd.estado_inicial)+'\n'+'Aceptacion: '+str(afd.estados_de_aceptacion)+'\n'+'Transiciones: '+str(trans)+'\n'
+
+def AFN_toString(afn: AFN):
+        trans=""
+        tablita = afn.transitions
+        for r in tablita:
+            for b in tablita.get(r):
+                trans += (f"({r}, {b}, {tablita.get(r).get(b)})- ")
+        return 'Estados: '+str(afn.estados)+'\n'+'Simbolos: '+str(afn.alfabeto)+'\n'+'Estado inicial: '+str(afn.estado_inicial)+'\n'+'Aceptacion: '+str(afn.estados_de_aceptacion)+'\n'+'Transiciones: '+str(trans)+'\n'
 def writeAFN(afn: AFN):
         f=open("AFN.txt",'w',encoding="utf-8")
         trans=""
